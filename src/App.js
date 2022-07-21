407cfa5d65a39af91a389330a72cb0dc6d8b0ae4
mikoogla9 <rgmikoogla@gmail.com>
2022-07-21 14:06:04 +0200

    new footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import "./App.css";
 import Content from "./components/Content/Content";
 import Navbar from "./components/Navbar/Navbar";
 import styles from "./App.module.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className={styles.main}>
-      <Navbar />
-      <Content />
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Content />
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return <div className={styles.main}>Copyright © 2022 mikoogla</div>;
+}
